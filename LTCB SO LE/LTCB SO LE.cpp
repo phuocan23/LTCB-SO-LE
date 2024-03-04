4eaac9daa031e3298f5a0d541a38a8efac6438e4
ADMIN <ADMIN@LAB1-MAY22>
2024-03-04 11:11:41 +0700

    Add project files.

---
+++ LTCB SO LE/LTCB SO LE.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int so, tongle = 0;
+	cout << "Nhap so :";
+	cin >> so;
+	for (int i = 0; i <= so; i++) {
+		if (i % 2 != 0)
+		{
+			cout << i << " ";
+			tongle = tongle + i;
+		}
+	}
+	cout << "\nTong cac so le tu 0 don" << so << "la" << tongle;
+	return 0;
+	}
